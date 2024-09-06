09166bd3cd0f61ad2cab240bc1f2a1f541385aa3
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2024-09-06 17:24:49 -0300

    feat: Refactoring TagsInputV4 demo

--- demos/src/components/TagsInput/TagsInputV4.vue
+++ demos/src/components/TagsInput/TagsInputV4.vue
@@ -1,5 +1,5 @@
-<script setup lang="ts">
-// DEPENDENCIES
+<script lang="ts">
+import { Fragment } from 'vue';
 import { v4 as uuidv4 } from 'uuid';
 
 // TYPES
@@ -9,79 +9,94 @@ import type {
   TagsModel,
 } from '@components/TagsInput/types';
 
-defineOptions({
+export default defineComponent({
+  name: 'TagsInputV4',
+
   inheritAttrs: false,
-});
 
-const tags = defineModel<TagsModel[]>();
-
-const newTag = ref<string>('');
-
-const inputBindings = reactive<TagsInputBindings>({
-  value: newTag.value,
-});
-
-// STATIC
-const inputListeners: TagsInputListeners = {
-  input: (e: Event) => handleInput(e),
-
-  keydown: (e: KeyboardEvent) => {
-    if (e.key === 'Backspace') {
-      removeTag();
-
-      return;
-    }
-
-    if (e.key === 'Enter') {
-      e.preventDefault();
-
-      addTag(newTag.value);
-    }
+  model: {
+    prop: 'modelValue',
+    event: 'update:modelValue',
   },
-};
 
-// METHODS
-function handleInput(event: Event): void {
-  newTag.value = (event.target as HTMLInputElement).value;
-}
+  emits: [
+    'update:modelValue',
+  ],
 
-function addTag(text: string): void {
-  const tagAlreadyExists = tags.value?.find(tag => tag.text === text);
+  setup(_, { slots, emit }) {
+    const tags = ref<TagsModel[]>([]);
 
-  if (newTag.value.length === 0 || tagAlreadyExists) {
-    return;
-  }
+    const newTag = ref<string>('');
 
-  tags.value?.push({
-    id: uuidv4(),
-    text,
-  });
+    const inputBindings = reactive<TagsInputBindings>({
+      value: newTag.value,
+    });
 
-  newTag.value = '';
-}
+    const inputListeners: TagsInputListeners = {
+      input: (e: Event) => handleInput(e),
 
-function removeTag(id?: string): void {
-  if (newTag.value.length || !tags.value) {
-    return;
-  }
+      keydown: (e: KeyboardEvent) => {
+        if (e.key === 'Backspace') {
+          removeTag();
 
-  if (!id && newTag.value.length === 0) {
-    tags.value.pop();
+          return;
+        }
 
-    return;
-  }
+        if (e.key === 'Enter') {
+          e.preventDefault();
 
-  const target = tags.value.findIndex(tag => tag.id === id);
+          addTag(newTag.value);
+        }
+      },
+    };
 
-  tags.value.splice(target, 1);
-}
+    function handleInput(event: Event): void {
+      newTag.value = (event.target as HTMLInputElement).value;
+    }
+
+    function addTag(text: string): void {
+      const tagAlreadyExists = tags.value?.find(tag => tag.text === text);
+
+      if (newTag.value.length === 0 || tagAlreadyExists) {
+        return;
+      }
+
+      tags.value?.push({
+        id: uuidv4(),
+        text,
+      });
+
+      newTag.value = '';
+
+      emit('update:modelValue', tags.value);
+    }
+
+    function removeTag(id?: string): void {
+      if (newTag.value.length || !tags.value) {
+        return;
+      }
+
+      if (!id && newTag.value.length === 0) {
+        tags.value.pop();
+
+        return;
+      }
+
+      const target = tags.value.findIndex(tag => tag.id === id);
+
+      tags.value.splice(target, 1);
+    }
+
+    return () => {
+      return h(Fragment, [
+        slots.default?.({
+          tags: tags.value,
+          inputBindings,
+          inputListeners,
+          removeTag,
+        }),
+      ]);
+    };
+  },
+});
 </script>
-
-<template>
-  <slot
-    :tags
-    :input-bindings
-    :input-listeners
-    :remove-tag
-  />
-</template>
